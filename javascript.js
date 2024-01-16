b31279cbfb83be1f2c3acb2a8b55e4df00cfe6e8
aakarshangupta <aakarshangup@gmail.com>
2024-01-16 22:53:37 +0530

    update1

--- javascript.js
+++ javascript.js
@@ -1,25 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var contactLink = document.getElementById("contact-link");
+    var discountLink = document.getElementById("discount-link");
 
-    contactLink.addEventListener("click", function (event) {
+    discountLink.addEventListener("click", function (event) {
         event.preventDefault();
 
-        // Scroll smoothly to the contact section
-        document.querySelector('#contact').scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    var contactLink = document.getElementById("contact-link");
-
-    contactLink.addEventListener("click", function (event) {
-        event.preventDefault();
-
-        // Scroll smoothly to the contact section
         document.querySelector('#Discount').scrollIntoView({
             behavior: 'smooth'
         });
     });
+
+    var contactButton = document.getElementById("contact-link");
+
+    contactButton.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        document.querySelector('#Contact Us').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+
 });
