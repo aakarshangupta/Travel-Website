88f86aca8e83fdf2f579c597ae6d7ea2166c1a4e
aakarshangupta <aakarshangup@gmail.com>
2024-01-18 19:20:34 +0530

    update

--- connect.php
+++ connect.php
@@ -1,3 +1,21 @@
 <?php
-    echo "this is";
+    $username = $_POST['username'];
+    $password = $_POST['password'];                     
+    $email = $_POST['email'];
+
+
+    $conn = new mysqli('localhost','root','','test');
+    if ($conn->connect_error) {
+        die('Connection Failed : '. $conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into registration(username, email, password)
+            values(?, ?, ?)");
+        $stmt->bind_param("sss",$username, $email, $password);
+        $stmt->execute();
+        echo "registration SUccessfully...";
+        $stmt->close();
+        $conn->close() ;
+
+    }
+
 ?>
